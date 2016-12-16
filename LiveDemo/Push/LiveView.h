409629e9d90fb44ec4ea43f701596d40eef22510
Cristo <liuqing1020liuqing@126.com>
2016-12-16 20:28:25 +0800

    功能完善版本

---
+++ LiveDemo/Push/LiveView.h
@@ -0,0 +1,13 @@
+//
+//  LiveView.h
+//  LiveDemo
+//
+//  Created by 刘清 on 2016/12/16.
+//  Copyright © 2016年 刘清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiveView : UIView
+
+@end
---
+++ LiveDemo/Push/LiveView.m
@@ -0,0 +1,21 @@
+//
+//  LiveView.m
+//  LiveDemo
+//
+//  Created by 刘清 on 2016/12/16.
+//  Copyright © 2016年 刘清. All rights reserved.
+//
+
+#import "LiveView.h"
+
+@implementation LiveView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
